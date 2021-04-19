26486d3a8c1a15cd1fe96d507d8eeaf50d5ab050
Luke Smith <luke@lukezsmith.com>
2021-04-19 17:28:16 +0100

    lab10 completed

--- Lab10_TrafficLight/TableTrafficLight.c
+++ Lab10_TrafficLight/TableTrafficLight.c
@@ -21,10 +21,6 @@
 #include "TExaS.h"
 #include "tm4c123gh6pm.h"
 
-// Pin definitions
-#define GPIO_PORTE_IN								(*((volatile unsigned long *)0x4002401C))	// bits 0-2
-
-
 // ***** 2. Global Declarations Section *****
 
 // FUNCTION PROTOTYPES: Each subroutine defined
@@ -47,33 +43,59 @@ struct State {
 typedef const struct State STyp;
 
 // State definitions
-#define goSouth		0
-#define waitSouth	1
-#define goWest		2
-#define waitWest	3
+#define goWest		0
+#define waitWest	1
+#define goSouth		2
+#define waitSouth	3
 #define walk			4
 #define allOff1		5
 #define allOff2		6
 #define dontWalk1	7
 #define dontWalk2	8
 
-// ***** 3. Subroutines Section *****
-int main(void){ 
-	STyp FSM[9] = {
-		{0x21, 0x2, 100, {goSouth, waitSouth, goSouth, waitSouth, waitSouth, waitSouth, waitSouth, waitSouth}},
-		{0x22, 0x2, 75, {waitSouth, goWest, goSouth, goWest, walk, walk, walk, walk}},
-		{0xC, 0x2, 100, {goWest, goWest, waitWest, waitWest, waitWest, waitWest, waitWest, waitWest}},
-		{0x14, 0x2, 75, {waitWest, goWest, goSouth, goSouth, walk, walk, walk, goSouth}},
-		{0x24, 0x8, 100, {walk, dontWalk1, dontWalk1, dontWalk1, walk, dontWalk1, dontWalk1, dontWalk1}},
-		{0x24, 0x2, 50, {dontWalk1, dontWalk2, dontWalk2, dontWalk2, walk, dontWalk2, dontWalk2, dontWalk2}},
-		{0x24, 0, 50, {dontWalk2, allOff1, allOff1, allOff1, walk, allOff1, allOff1, allOff1}},
-		{0x24, 0x2, 50, {allOff1, allOff2, allOff2, allOff2, walk, allOff2, allOff2, allOff2}},
-		{0x24, 0, 50, {allOff2, goWest, goSouth, goWest, walk, goWest, goSouth, goWest}},
-	};
+STyp FSM[9] = {
+	{0x0C, 0x02, 100, {goWest, goWest, waitWest, waitWest, waitWest, waitWest, waitWest, waitWest}},
+	{0x14, 0x02, 60, {waitWest, goWest, goSouth, goSouth, walk, walk, goSouth, goSouth}},
+	{0x21, 0x02, 100, {goSouth, waitSouth, goSouth, waitSouth, waitSouth, waitSouth, waitSouth, waitSouth}},
+	{0x22, 0x02, 60, {waitSouth, goWest, goSouth, goWest, walk, walk, walk, walk}},		
+	{0x24, 0x08, 100, {walk, dontWalk1, dontWalk1, dontWalk1, walk, dontWalk1, dontWalk1, dontWalk1}},
+	{0x24, 0x02, 60, {dontWalk1, dontWalk2, dontWalk2, dontWalk2, dontWalk2, dontWalk2, dontWalk2, dontWalk2}},
+	{0x24, 0x00, 60, {dontWalk2, allOff1, allOff1, allOff1, allOff1, allOff1, allOff1, allOff1}},
+	{0x24, 0x02, 60, {allOff1, allOff2, allOff2, allOff2, allOff2, allOff2, allOff2, allOff2}},
+	{0x24, 0x00, 60, {allOff2, goWest, goSouth, goWest, walk, goWest, goSouth, goWest}},
+};
 
-	unsigned long currentState = allOff2;	// index to current state
-	unsigned long Input;
+unsigned long currentState;	// index to current state
+unsigned long Input;
+
+
+
+// ***** 3. Subroutines Section *****
+
+void PortBEF_Init(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x32;      				// 1) B E F
+  delay = SYSCTL_RCGC2_R;      				// 2) no need to unlock
 	
+  GPIO_PORTE_AMSEL_R 	&= ~0x07; 			// 3) disable analog function on PE2-0
+  GPIO_PORTE_PCTL_R 	&= ~0x00000FFF; 	// 4) enable regular GPIO
+  GPIO_PORTE_DIR_R 		&= ~0x07; 			// 5) inputs on PE2-0
+  GPIO_PORTE_AFSEL_R 	&= ~0x07; 			// 6) regular function on PE2-0
+  GPIO_PORTE_DEN_R 		|=  0x07; 			// 7) enable digital on PE2-0
+
+  GPIO_PORTB_AMSEL_R 	&= ~0x3F; 			// 3) disable analog function on PB5-0
+  GPIO_PORTB_PCTL_R 	&= ~0x00FFFFFF; 	// 4) enable regular GPIO
+  GPIO_PORTB_DIR_R 		|=  0x3F;    		// 5) outputs on PB5-0
+  GPIO_PORTB_AFSEL_R 	&= ~0x3F; 			// 6) regular function on PB5-0
+  GPIO_PORTB_DEN_R 		|=  0x3F;    		// 7) enable digital on PB5-0
+
+  GPIO_PORTF_AMSEL_R 	&= ~0x0A;       	// 3) disable analog on PF1, PF3
+  GPIO_PORTF_PCTL_R 	&= ~0x0000F0F0; 	// 4) PCTL GPIO on PF1, PF3
+  GPIO_PORTF_DIR_R 		|=  0x0A;       	// 5) PF3, PF1 out
+  GPIO_PORTF_AFSEL_R 	&= ~0x0A;       	// 6) disable alt funct on PF1, PF3
+  GPIO_PORTF_DEN_R 		|=  0x0A;       	// 7) enable digital I/O on PF1, PF3
+}
+
+int main(void){ 
 	TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210,ScopeOff); // activate grader and set system clock to 80 MHz
 	
 	initPortB();	
@@ -81,14 +103,15 @@ int main(void){
 	initPortF(); 
 	
 	SysTick_Init();
-	
+
+	currentState = goWest;
   EnableInterrupts();
 	
   while(1){
 		GPIO_PORTB_DATA_R = FSM[currentState].carLeds;			// output car leds
-		GPIO_PORTF_DATA_R = FSM[currentState].pedLeds; 		// output pedestrian leds
+		GPIO_PORTF_DATA_R = FSM[currentState].pedLeds; 			// output pedestrian leds
+
 		SysTick_Wait10ms(FSM[currentState].Time);						// delay
-		//Input = GPIO_PORTE_IN;															// get sensor input
 		Input = GPIO_PORTE_DATA_R & 0x7;
 		currentState = FSM[currentState].nextState[Input];	// advance to next state
   }
@@ -98,45 +121,43 @@ void initPortB(void){
 	unsigned long delay;
 	SYSCTL_RCGC2_R |= 0x2;								// 1 - activate clock for port
 	delay = SYSCTL_RCGC2_R;								// 2 - allow time for clock to start
-	//GPIO_PORTB_AMSEL_R &= ~0x0000003F; 		// 3 - disable analog for pins
-	//GPIO_PORTB_PCTL_R &= ~0x0000003F;			// 4 - PCTL GPIO on pins
-	GPIO_PORTB_DIR_R |= 0x0000003F;				// 5 - pin directions
-	//GPIO_PORTB_AFSEL_R &= ~0x0000003F;	 	// 6 - disable alt functions for pins
-	GPIO_PORTB_DEN_R |= 0x0000003F;				// 7 - enable digital I/O for pins
+	GPIO_PORTB_AMSEL_R &= ~0x3F; 					// 3 - disable analog for pins
+	GPIO_PORTB_PCTL_R &= ~0x00FFFFFF;			// 4 - PCTL GPIO on pins
+	GPIO_PORTB_DIR_R |= 0x3F;							// 5 - pin directions
+	GPIO_PORTB_AFSEL_R &= ~0x3F;	 				// 6 - disable alt functions for pins
+	GPIO_PORTB_DEN_R |= 0x3F;							// 7 - enable digital I/O for pins
 }
 
 void initPortE(void){
 	unsigned long delay;
 	SYSCTL_RCGC2_R |= 0x10;								// 1 - activate clock for port
 	delay = SYSCTL_RCGC2_R;								// 2 - allow time for clock to start
-	//GPIO_PORTE_AMSEL_R &= ~0x00000007; 		// 3 - disable analog for pins
-	//GPIO_PORTE_PCTL_R &= ~0x0000007;			// 4 - PCTL GPIO on pins
-	//GPIO_PORTE_DIR_R &= ~0x00000007;			// 5 - pin directions
-	//GPIO_PORTE_AFSEL_R &= ~0x0000007;	 		// 6 - disable alt functions for pins
-	GPIO_PORTE_DEN_R |= 0x0000007;				// 7 - enable digital I/O for pins
+	GPIO_PORTE_AMSEL_R &= ~0x07; 					// 3 - disable analog for pins
+	GPIO_PORTE_PCTL_R &= ~0x00000FFF;			// 4 - PCTL GPIO on pins
+	GPIO_PORTE_DIR_R &= ~0x07;						// 5 - pin directions
+	GPIO_PORTE_AFSEL_R &= ~0x07;	 				// 6 - disable alt functions for pins
+	GPIO_PORTE_DEN_R |= 0x07;							// 7 - enable digital I/O for pins
 }
 
 void initPortF(void){
 	unsigned long delay;
 	SYSCTL_RCGC2_R |= 0x20;								// 1 - activate clock for port
 	delay = SYSCTL_RCGC2_R;								// 2 - allow time for clock to start
-	//GPIO_PORTB_AMSEL_R &= ~0x0000000A; 		// 3 - disable analog for pins
-	//GPIO_PORTB_PCTL_R &= ~0x0000000A;			// 4 - PCTL GPIO on pins
-	GPIO_PORTB_DIR_R |= 0x0000000A;				// 5 - pin directions
-	//GPIO_PORTB_AFSEL_R &= ~0x0000000A;	 	// 6 - disable alt functions for pins
-	GPIO_PORTB_DEN_R |= 0x0000000A;				// 7 - enable digital I/O for pins
+	GPIO_PORTF_AMSEL_R &= ~0x0A; 					// 3 - disable analog for pins
+	GPIO_PORTF_PCTL_R &= ~0x0000F0F0;			// 4 - PCTL GPIO on pins
+	GPIO_PORTF_DIR_R |= 0x0A;							// 5 - pin directions
+	GPIO_PORTF_AFSEL_R &= ~0x0A;	 				// 6 - disable alt functions for pins
+	GPIO_PORTF_DEN_R |= 0x0A;							// 7 - enable digital I/O for pins
 }
 
 void SysTick_Init(void){
-	//NVIC_ST_RELOAD_R = 79999999;
-	//NVIC_ST_CURRENT_R = 0;
 	NVIC_ST_CTRL_R = 0;						// disable SysTick during setup
 	NVIC_ST_CTRL_R = 0x00000005;	// enable SysTick with core clock
 }
 
 void SysTick_Wait(unsigned long delay){
-	NVIC_ST_RELOAD_R =  delay-1;	// number of counts to wait
-	NVIC_ST_CURRENT_R = 0;				// any value written to CURRENT clears it
+	NVIC_ST_RELOAD_R =  delay-1;						// number of counts to wait
+	NVIC_ST_CURRENT_R = 0;									// any value written to CURRENT clears it
 	while((NVIC_ST_CTRL_R&0x00010000)==0){}	// wait for count flag
 }
 
@@ -146,3 +167,5 @@ void SysTick_Wait10ms(unsigned long delay){
 		SysTick_Wait(800000);	// wait 10ms
 	}
 }
+
+
